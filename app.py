b2c7507e76641926f67e90c9b6be67f2054dd754
keyframesfound <80465725+keyframesfound@users.noreply.github.com>
2025-03-11 20:17:49 +0800

    Removed diskspace

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def index():
             'cpu_percent': psutil.cpu_percent(interval=1),
             'memory': psutil.virtual_memory()._asdict(),
             'temperature': get_cpu_temperature(),
-            'disk': psutil.disk_usage('/')
         }
     return render_template('index.html', tab=tab, stats=system_stats)
 
